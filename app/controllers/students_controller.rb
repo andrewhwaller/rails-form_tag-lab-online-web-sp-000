95a1047be67ab918cd29072d69777091784f504c
Andrew Waller <andrewhwaller@gmail.com>
2019-06-04 17:29:08 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,15 +5,14 @@ class StudentsController < ApplicationController
   end
 
   def show
-    @student = Student.find(params[:id])
   end
 
   def new
-    @student = Student.new
   end
 
   def create
-    @student = Student.find_by(params[:first_name])
+    session[:form_params] = params.inspect
+    redirect_to new_student_path
   end
 
 end
